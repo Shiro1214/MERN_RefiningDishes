af4ca4ab6d121926a8aada116a024d1dcf4f532d
shiroinhawai <henglongsun7@gmail.com>
2023-12-12 11:26:52 -1000

    Set up Mongo Atlas and Mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 
 const express = require('express') 
-
+const mongoose = require('mongoose')
 const recipesRoutes = require('./routes/recipes')
 //express app
 const app = express()
@@ -21,7 +21,13 @@ app.use((req, res, next) => {
 // routes - certain endpoints (last middleware to execute)
 app.use('/api/recipes', recipesRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Listening . . .')
-})
+//Connect to db - Async
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log('Connected to DB and Listening on port', process.env.PORT)
+    })
+}).catch((error) => {
+    console.log(error);
+}) 
